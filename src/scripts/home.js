16eea7bbbb7bed35b9086f449a7d5ed3c81bcf77
HenryBautista <henrybautistahtt@hotmail.com>
2023-12-21 16:30:45 -0400

    Add points selection

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
+
+    $('#points').on('click', '.circle-button-mid', function() {
+        $('.circle-button-mid').removeClass('selected')
+        $(this).addClass('selected')
+    })
     
 });
